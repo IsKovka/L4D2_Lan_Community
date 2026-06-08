21d0f31f73b206657202c48360dd9609b0fa0b67
IsKovka <mcchingenwings@web.de>
2026-06-09 01:47:54 +0200

    Update 5event_youtube.js

--- eventfolder/events/5summerjam2026/5event_youtube.js
+++ eventfolder/events/5summerjam2026/5event_youtube.js
@@ -1,39 +1,39 @@
 const campaignMeta = {
 
     "Infektionszentrum": {
-        youtube: "https://www.youtube.com/watch?v=aWW1uR82dUo&list=PLMMjH3eMWTg03VBOZ0W6aWYrqMmQUv4H2&index=1&t=785s"
+        youtube: ""
     },
 
     "Die Brücke": {
-        youtube: "https://www.youtube.com/watch?v=1JsqMDMpevY&list=PLMMjH3eMWTg03VBOZ0W6aWYrqMmQUv4H2&index=2"
+        youtube: ""
     },
 
     "Dunkler Karneval": {
-        youtube: "https://www.youtube.com/watch?v=dBTyvf-f7cQ&list=PLMMjH3eMWTg03VBOZ0W6aWYrqMmQUv4H2&index=3&t=2894s"
+        youtube: ""
     },
 
     "Sumpffieber": {
-        youtube: "https://www.youtube.com/watch?v=hvmus2tzwKc&list=PLMMjH3eMWTg03VBOZ0W6aWYrqMmQUv4H2&index=4&t=1932s"
+        youtube: ""
     },
 
     "Sturmflut": {
-        youtube: "https://www.youtube.com/watch?v=CZu-qocUNJI&list=PLMMjH3eMWTg03VBOZ0W6aWYrqMmQUv4H2&index=5&t=12s"
+        youtube: ""
     },
 
     "Die Gemeinde": {
-        youtube: "https://www.youtube.com/watch?v=4AMuj8FGQUY&list=PLMMjH3eMWTg03VBOZ0W6aWYrqMmQUv4H2&index=6&t=7s"
+        youtube: ""
     },
 
     "The Sacrifice": {
-        youtube: "https://www.youtube.com/watch?v=72On5As4i6U&list=PLMMjH3eMWTg03VBOZ0W6aWYrqMmQUv4H2&index=7&t=1s"
+        youtube: ""
     },
 
     "Mercy Hospital": {
-        youtube: "https://www.youtube.com/watch?v=2kD2g4JKszM&list=PLMMjH3eMWTg03VBOZ0W6aWYrqMmQUv4H2&index=8&t=1041s"
+        youtube: ""
     },
 
     "Crash Course": {
-        youtube: "https://www.youtube.com/watch?v=W8w9oZNouNw&list=PLMMjH3eMWTg03VBOZ0W6aWYrqMmQUv4H2&index=9"
+        youtube: ""
     },
 
     "Todeszone": {
