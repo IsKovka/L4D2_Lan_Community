78ecaaeb7bc6032b4b9abdb8a09833d2d0d0d73c
IsKovka <mcchingenwings@web.de>
2026-06-14 16:19:49 +0200

    Add files via upload

---
+++ eventfolder/events/8summerjam2027/8event_youtube.js
@@ -0,0 +1,58 @@
+const campaignMeta = {
+
+    "Infektionszentrum": {
+        youtube: ""
+    },
+
+    "Die Brücke": {
+        youtube: ""
+    },
+
+    "Dunkler Karneval": {
+        youtube: ""
+    },
+
+    "Sumpffieber": {
+        youtube: ""
+    },
+
+    "Sturmflut": {
+        youtube: ""
+    },
+
+    "Die Gemeinde": {
+        youtube: ""
+    },
+
+    "The Sacrifice": {
+        youtube: ""
+    },
+
+    "Mercy Hospital": {
+        youtube: ""
+    },
+
+    "Crash Course": {
+        youtube: ""
+    },
+
+    "Todeszone": {
+        youtube: ""
+    },
+
+    "Flug zur Hölle": {
+        youtube: ""
+    },
+
+    "Blutdurst": {
+        youtube: ""
+    },
+
+    "Kalter Strom": {
+        youtube: ""
+    },
+
+    "Das letzte Gefecht": {
+        youtube: ""
+    },
+};
